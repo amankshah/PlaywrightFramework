ee86fcf8fbfa488c6dcdc7e843a4f160a90a573a
your-git-username <your-email@example.com>
2024-08-16 21:24:35 +0530

    Multi window handling , Synchronous methods using promise.all

--- tests/UiBasicsTest.spec.js
+++ tests/UiBasicsTest.spec.js
@@ -37,3 +37,31 @@ test("Second Test", async ({ page }) => {
   //   expect(await page.title()).toBe("Google");
   await expect(page).toHaveTitle("Google");
 });
+
+test("Getting Product name from UI", async ({ page }) => {
+  await page.goto(
+    "https://ecommerceforautomationtesting.glowingdark.com/shop/"
+  );
+
+  let productName = await page
+    .locator("h3.wp-block-post-title")
+    .nth(0)
+    .textContent();
+  // console.log("Product Name: " + productName);
+
+  //print all product names
+  let productNames = await page
+    .locator("h3.wp-block-post-title")
+    .allTextContents();
+  // console.log(productNames);
+
+  //print last product name
+  let lastProductName = await page
+    .locator("h3.wp-block-post-title")
+    .last()
+    .textContent();
+  console.log(lastProductName);
+});
+
+
+
