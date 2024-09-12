a3553049800053e22d9f6df57b5824540d144489
your-git-username <your-email@example.com>
2024-09-12 20:11:14 +0530

    Video Recording

--- playwright.config1.js
+++ playwright.config1.js
@@ -21,9 +21,9 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -55,6 +55,7 @@ module.exports = defineConfig({
         permissions: ["geolocation"], // giving permission for geolocation
         //using always headed mode
         headless: false,
+        video: "retain-on-failure", //this will record the video 
 
         // viewport: { width: 720, height: 720 },
       },
