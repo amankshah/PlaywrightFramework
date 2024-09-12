8ca1d37bca01a4fce06b19ecb516987cb6430b37
your-git-username <your-email@example.com>
2024-09-12 19:17:42 +0530

    Parameterized Purchase Using Fixers

---
+++ Utils/test-base.js
@@ -0,0 +1,9 @@
+const base = require("@playwright/test");
+
+exports.customTest = base.test.extend({
+  testDataForOrder: {
+    username: "testtopro@gmail.com",
+    password: "Aman@1992",
+    product: "ZARA COAT 3",
+  },
+});
